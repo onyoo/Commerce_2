ce7c385ca7e15ed16eee040e5e8bf042264bf3f9
onyoo <roberto9m9@gmail.com>
2016-04-15 13:01:17 +0200

    adds working productItem component

--- app/assets/javascripts/app/components/ProductItem.js
+++ app/assets/javascripts/app/components/ProductItem.js
@@ -1,7 +1,22 @@
+var ProductItem = {
+  template: [
+    '<p>',
+      '<a ui-sref="home.product({name: productItem.product.name, id: productItem.product.id})">',
+      '{{productItem.product.name}}',
+      '</a>',
+    '</p>',
+    '<p>{{productItem.product.price | currency}}</p>',
+    '<p>Only {{productItem.product.inventory}} left!</p><br/>'
+  ].join(''),
+  bindings: {
+    product: '='
+  },
+  controller: function() {
+    var ctrl = this;
+  },
+  controllerAs: 'productItem'
+}
+
 angular
   .module('app')
   .component('productItem', ProductItem);
-
-function ProductItem() {
-
-}
